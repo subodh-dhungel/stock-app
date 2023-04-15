26f5a10e03420f81a5f37c6c471a7465e7d63786
unknown <subodhdhungel20@gmail.com>
2023-04-15 07:23:30 +0545

    Minor Changes

---
+++ components/home/FeaturedNews.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Grid, Paper } from '@mui/material'
+import CustomHeading from '../CustomHeading'
+import NewsCard from '../NewsCard'
+
+
+const FeaturedNews = () => {
+    const xs = 12
+    const sm = 6
+    const md = 6
+    const lg = 4
+    const xl = 4
+    return (
+        <Paper
+            variant='elevation'
+            elevation={8}
+            sx={{
+                padding: 5
+            }}
+        >
+            <Grid
+                container
+                spacing={3}
+            >
+                <Grid item xs={xs}>
+                    <CustomHeading title='Exclusive News' />
+                </Grid>
+
+                <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                    <NewsCard />
+                </Grid>
+
+                <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                    <NewsCard />
+                </Grid>
+
+                <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                    <NewsCard />
+                </Grid>
+
+                <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                    <NewsCard />
+                </Grid>
+
+                <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                    <NewsCard />
+                </Grid>
+
+                <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                    <NewsCard />
+                </Grid>
+            </Grid>
+        </Paper>
+    )
+}
+
+export default FeaturedNews
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,17 +1,11 @@
-import CustomHeading from '@/components/CustomHeading'
 import Navigation from '@/components/Navigation'
-import NewsCard from '@/components/NewsCard'
 import BullionPrice from '@/components/home/BullionPrice'
+import FeaturedNews from '@/components/home/FeaturedNews'
 import Inflation from '@/components/home/Inflation'
 import { useTheme } from '@emotion/react'
-import { Container, Grid, Paper, Typography } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 
-const Homepage = (props) => {
-  const xs = 12
-  const sm = 6
-  const md = 6
-  const lg = 4
-  const xl = 4
+const Homepage = () => {
   const theme = useTheme()
 
   return (
@@ -31,60 +25,13 @@ const Homepage = (props) => {
               marginTop: 10
             },
             [theme.breakpoints.up('lg')]: {
-              padding: 5,
+              padding: 0,
               marginTop: 10
             },
           }}
         >
-          {/* Introduction
-          <Grid container>
-            <Grid item xs={xs} md={12} lg={12} xl={12}>
-              <Introduction />
-            </Grid>
-          </Grid> */}
 
-          {/*Featured News Cards*/}
-          <Paper 
-            variant='elevation'
-            elevation={8}
-            sx={{
-              padding: 5
-            }}
-          >
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid item xs={xs}>
-              <CustomHeading title='Exclusive News'/>
-            </Grid>
-
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <NewsCard />
-            </Grid>
-
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <NewsCard />
-            </Grid>
-
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <NewsCard />
-            </Grid>
-
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <NewsCard />
-            </Grid>
-
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <NewsCard />
-            </Grid>
-
-            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
-              <NewsCard />
-            </Grid>
-          </Grid>
-          </Paper>
-          
+          <FeaturedNews />
           <BullionPrice />
           <Inflation />
 
